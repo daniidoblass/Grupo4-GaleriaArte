da7f8836a85b5abb48357289e0a1b78807bb65ea
samuelacostasanjose <samuelacostafernandez.sanjose@alumnado.fundacionloyola.net>
2022-12-14 10:49:52 +0100

    LIBRERIA

---
+++ MokeApp/src/controlador/controladorClienteFTP.java
@@ -0,0 +1,38 @@
+package controlador;
+
+import org.apache.commons.net.ftp.FTPClient;
+
+public class controladorClienteFTP {
+
+	private FTPClient cliente;
+	private String servidorFTP = "ftpupload.net";
+	private String usuario = "b3_33188433";
+	private String contrasena = "2wknvh9m";
+	
+	public controladorClienteFTP() {
+		
+		try {
+			
+			// Crear cliente ftp
+			cliente = new FTPClient();
+
+			// conectarse a ese servidor
+			cliente.connect(servidorFTP);
+
+			// iniciar sesión en el servidor
+			boolean loginCorrecto = cliente.login(usuario, contrasena);
+			if(!loginCorrecto) {
+				System.out.print("ERROR: usuario o contraseña incorrectas");
+				cliente.disconnect();
+			}
+
+		}
+		catch(Exception e) {
+			System.out.println("ERROR: no se ha podido establecer conexion con el Cliente FTP");
+		}
+	}
+	
+	public FTPClient getFTPClient() {
+		return cliente;
+	}
+}
