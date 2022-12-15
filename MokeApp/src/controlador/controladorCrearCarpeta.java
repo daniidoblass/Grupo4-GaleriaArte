2664637ba696dc352f9f7bf8477facd3340f4478
daniidoblass <danieldoblasflorido.sanjose@alumnado.fundacionloyola.net>
2022-12-15 13:24:07 +0100

    Commit modificacion de crear Carpeta

--- MokeApp/src/controlador/controladorCrearCarpeta.java
+++ MokeApp/src/controlador/controladorCrearCarpeta.java
@@ -8,7 +8,10 @@ package controlador;
 
 import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 import conexion.Conexion;
 import modelo.Modelo;
@@ -24,14 +27,65 @@ public class controladorCrearCarpeta {
 	    private FTPClient cliente;
 	    private Eventos eventos;
 	    private Conexion conexion;
+	    private String nombreCarpeta;
 	    
 	    public controladorCrearCarpeta(Modelo modelo, Vista vista, Eventos eventos, Conexion conexion, FTPClient cliente) throws IOException {
 	    	
+	    	this.cliente = cliente;
 	    	this.modelo = modelo;
 	        this.vista = vista;
 	        this.eventos = eventos;
-	        vistaCrearCarpeta = new VistaCrearCarpeta(modelo,cliente);
+	        vistaCrearCarpeta = new VistaCrearCarpeta(modelo);
 	        this.conexion = conexion;
+	        crearCarpeta();
+	        
+	        
 
 	    }
+	    
+	    private void crearCarpeta() throws IOException {
+	    	
+	    	String direcSelec = cliente.printWorkingDirectory();
+	    	
+	    	nombreCarpeta = vistaCrearCarpeta.crearNombreCarpeta();
+			
+			if(!(nombreCarpeta == null)) {
+				
+				String directorio = direcSelec; //Cambiar por direcSelec
+				
+				if(!direcSelec.equals("/")) { // directorio --> direcSelec.
+					
+					directorio = directorio + " que tal?";
+				}
+				
+				directorio += nombreCarpeta.trim(); //quita los espacios en blanco a der e izq.
+
+				try {
+					
+					if(cliente.makeDirectory(directorio)) {
+						String m = nombreCarpeta.trim()+"Se ha creado correctamente...";
+						JOptionPane.showMessageDialog(null, m);
+						
+						//Directorio de trabajo actual.
+						cliente.changeWorkingDirectory(direcSelec);
+						FTPFile [] ff2 = null;
+						
+						//Obtener ficheros del directorio actual.
+						ff2 = cliente.listFiles();
+						
+						//llenar la lista.
+						//llenarLista(ff2,direcSelec);
+						
+					}else {
+						JOptionPane.showMessageDialog(null,nombreCarpeta.trim()+ " => No se ha podido crear ...");
+					}
+					
+				}catch (Exception e) {
+					// TODO: handle exception
+					e.printStackTrace();
+				}
+			}
+	    }
+	    
+	    
 }
--- MokeApp/src/vista/VistaCrearCarpeta.java
+++ MokeApp/src/vista/VistaCrearCarpeta.java
@@ -19,6 +19,7 @@ import org.apache.commons.net.ftp.FTPFile;
 
 import conexion.Conexion;
 import controlador.Eventos;
+import controlador.controladorCrearCarpeta;
 
 import javax.swing.*;
 
@@ -26,49 +27,19 @@ import modelo.Modelo;
 
 public class VistaCrearCarpeta extends JFrame{
 	
+	private String nombreCarpeta;
+	private controladorCrearCarpeta controladorCrearCarpeta;
+	private Modelo modelo;
 
-	public VistaCrearCarpeta(Modelo modelo, FTPClient cliente) throws IOException {
+	public VistaCrearCarpeta(Modelo modelo) throws IOException {
 		
-		String nombreCarpeta = JOptionPane.showInputDialog(null,"Introducir nombre carpeta", "carpeta");
-		
-		String direcSelec = cliente.printWorkingDirectory();
-		
-		if(!(nombreCarpeta == null)) {
-			
-			String directorio = direcSelec; //Cambiar por direcSelec
-			
-			if(!direcSelec.equals("/")) { // directorio --> direcSelec.
-				
-				directorio = directorio + " que tal?";
-			}
-			
-			directorio += nombreCarpeta.trim(); //quita los espacios en blanco a der e izq.
+		this.modelo = modelo;
 
-			try {
-				
-				if(cliente.makeDirectory(directorio)) {
-					String m = nombreCarpeta.trim()+"Se ha creado correctamente...";
-					JOptionPane.showMessageDialog(null, m);
-					
-					//Directorio de trabajo actual.
-					cliente.changeWorkingDirectory(direcSelec);
-					FTPFile [] ff2 = null;
-					
-					//Obtener ficheros del directorio actual.
-					ff2 = cliente.listFiles();
-					
-					//llenar la lista.
-					//llenarLista(ff2,direcSelec);
-					
-				}else {
-					JOptionPane.showMessageDialog(null,nombreCarpeta.trim()+ " => No se ha podido crear ...");
-				}
-				
-			}catch (Exception e) {
-				// TODO: handle exception
-				e.printStackTrace();
-			}
-		}
+	}
+	
+	public String crearNombreCarpeta() {
+		
+		 return JOptionPane.showInputDialog(null,"Introducir nombre carpeta", "carpeta");
 	}
 
 	
