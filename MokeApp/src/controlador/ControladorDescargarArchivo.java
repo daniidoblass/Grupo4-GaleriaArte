81466b5659a033b175e1b2c4bda2d2122796e761
daniidoblass <danieldoblasflorido.sanjose@alumnado.fundacionloyola.net>
2022-12-15 13:42:43 +0100

    commit modificacion Descargar Carpeta

--- MokeApp/src/controlador/ControladorDescargarArchivo.java
+++ MokeApp/src/controlador/ControladorDescargarArchivo.java
@@ -1,5 +1,9 @@
 package controlador;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * @author Daniel Jesús Doblas Florido
  * @date 14/12/2022
@@ -9,6 +13,9 @@ package controlador;
 import java.io.IOException;
 import java.net.SocketException;
 
+import javax.swing.JFileChooser;
+
+import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
 import conexion.Conexion;
@@ -19,6 +26,7 @@ import vista.VistaSubirArchivo;
 
 public class ControladorDescargarArchivo {
 	
+	private FTPClient cliente;
 	private Modelo modelo;
 	private Vista vista;
 	private Eventos eventos;
@@ -27,11 +35,48 @@ public class ControladorDescargarArchivo {
 
 	public ControladorDescargarArchivo(Modelo modelo, Vista vista, Eventos eventos, Conexion conexion,FTPClient cliente) throws SocketException, IOException {
 		
+		this.cliente = cliente;
 		this.modelo = modelo;
 		this.vista = vista;
 		this.eventos = eventos;
-		vistaDescargarArchivo = new VistaDescargarArchivo(cliente);
+		vistaDescargarArchivo = new VistaDescargarArchivo(modelo);
 		this.conexion = conexion;
+		DescargarFichero();
 		
 	}
+	
+	private void DescargarFichero() throws SocketException, IOException {
+		
+		cliente.setFileType(FTP.BINARY_FILE_TYPE);
+
+		String archivoDelServidor = "Documento.docx";
+		
+        int returnVal = vistaDescargarArchivo.mostrarJFileChooser();
+
+        if (returnVal == vistaDescargarArchivo.getjFileChooser().APPROVE_OPTION) {
+        	File f = vistaDescargarArchivo.getjFileChooser().getSelectedFile();
+        	// obtener carpeta de destino
+        	String carpetaDestino = (f.getAbsolutePath()).toString();
+        	System.out.println("destino:" + carpetaDestino);
+        	// ubicación donde se va a descargar
+			BufferedOutputStream buffSalida = new BufferedOutputStream(
+					new FileOutputStream(carpetaDestino + "\\"+archivoDelServidor));
+
+			// Descargar el archivo.
+			boolean descargado = cliente.retrieveFile(archivoDelServidor, buffSalida); // Devuelve TRUE si se ha
+																						// descargado.
+
+			if (descargado) {
+
+				System.out.println("Se ha podido descargar.");
+				
+			} else {
+
+				System.out.println("No se ha podido descargar");
+			}
+			
+			//Cerrar el buffer.
+			buffSalida.close();
+        }
+}
 }
--- MokeApp/src/vista/VistaDescargarArchivo.java
+++ MokeApp/src/vista/VistaDescargarArchivo.java
@@ -17,64 +17,40 @@ import javax.swing.JFrame;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 
+import controlador.ControladorDescargarArchivo;
 import modelo.Modelo;
 
 public class VistaDescargarArchivo extends JFrame{
 
-	private FTPClient cliente;
-	public static String usuario = "b3_33188433";
-	public static String contrasena = "2wknvh9m";
-	public static String directorioTrabajo = "/";
+	private ControladorDescargarArchivo controladorDescargarArchivo;
+	private Modelo modelo;
+	private JFileChooser jFileChooser;
 	
-	public  VistaDescargarArchivo (FTPClient cliente) {
+	public  VistaDescargarArchivo (Modelo modelo) {
 
-		this.cliente = cliente;
-		try {
-			DescargarFichero();
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.modelo = modelo;
 		
 	}
 	
-	private void DescargarFichero() throws SocketException, IOException {
+	public int mostrarJFileChooser() {
 		
-			cliente.setFileType(FTP.BINARY_FILE_TYPE);
-
-			String archivoDelServidor = "Documento.docx";
-			
-	        JFileChooser jFileChooser = new JFileChooser();
-	        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-	        jFileChooser.setDialogTitle("Seleccione el Directorio donde DESCARGAR el fichero");
-	        int returnVal = jFileChooser.showDialog(null, "Descargar");
-
-	        if (returnVal == JFileChooser.APPROVE_OPTION) {
-	        	File f = jFileChooser.getSelectedFile();
-	        	// obtener carpeta de destino
-	        	String carpetaDestino = (f.getAbsolutePath()).toString();
-	        	System.out.println("destino:" + carpetaDestino);
-	        	// ubicación donde se va a descargar
-				BufferedOutputStream buffSalida = new BufferedOutputStream(
-						new FileOutputStream(carpetaDestino + "\\"+archivoDelServidor));
-
-				// Descargar el archivo.
-				boolean descargado = cliente.retrieveFile(archivoDelServidor, buffSalida); // Devuelve TRUE si se ha
-																							// descargado.
-
-				if (descargado) {
-
-					System.out.println("Se ha podido descargar.");
-					
-				} else {
-
-					System.out.println("No se ha podido descargar");
-				}
-				
-				//Cerrar el buffer.
-				buffSalida.close();
-	        }
+		jFileChooser = new JFileChooser();
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jFileChooser.setDialogTitle("Seleccione el Directorio donde DESCARGAR el fichero");
+        int returnVal = jFileChooser.showDialog(null, "Descargar");
+        
+		return returnVal;
 	}
+
+	public JFileChooser getjFileChooser() {
+		return jFileChooser;
+	}
+
+	public void setjFileChooser(JFileChooser jFileChooser) {
+		this.jFileChooser = jFileChooser;
+	}
+	
+
 	
 	
 }
