8a5a18a9f9647190b07c5e480d243c26b6f6b871
Pablopf66 <102785960+Pablopf66@users.noreply.github.com>
2022-12-16 08:35:10 +0100

    Creación de metodo recursivo para eliminar todo el contenido de la carpeta

--- MokeApp/src/vista/VistaEliminarCarpeta.java
+++ MokeApp/src/vista/VistaEliminarCarpeta.java
@@ -8,6 +8,7 @@ package vista;
 import javax.swing.*;
 
 import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPFile;
 
 import modelo.Modelo;
 
@@ -19,14 +20,15 @@ import java.io.IOException;
 public class VistaEliminarCarpeta extends JFrame {
 
 	private FTPClient cliente;
-	private String nombreArchivo = "mokemamahuevo";
+	private String nombreArchivo = "/MOKEFTP";
+	private String currentDir = "";
 
 	public VistaEliminarCarpeta(FTPClient cliente) {
 
 		this.cliente = cliente;
 
 		try {
-			eliminarCarpeta(nombreArchivo);
+			eliminarCarpeta(cliente, nombreArchivo, currentDir);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,14 +36,51 @@ public class VistaEliminarCarpeta extends JFrame {
 
 	}
 
-	private void eliminarCarpeta(String nombreArchivo) throws HeadlessException, IOException {
-		// TODO Auto-generated method stub
-		int opcion = JOptionPane.showConfirmDialog(null, "Deseas eliminar la carpeta "+nombreArchivo);
-		if (opcion == JOptionPane.YES_OPTION) {
-			JOptionPane.showMessageDialog(null,nombreArchivo+" se ha elimindado correctamente");
-		} else {
-
+	private void eliminarCarpeta(FTPClient cliente, String nombreArchivo, String currentDir)
+			throws HeadlessException, IOException {
+		cliente.changeWorkingDirectory(nombreArchivo);
+		String dirToList = nombreArchivo;
+		if (!currentDir.equals("")) {
+			dirToList += "/" + currentDir;
 		}
+		FTPFile[] subFiles = cliente.listFiles(dirToList);
+		if (subFiles != null && subFiles.length > 0) {
+			for (FTPFile aFile : subFiles) {
+				String currentFileName = aFile.getName();
+				if (currentFileName.equals(".") || currentFileName.equals("..")) {
+					// skip parent directory and the directory itself
+					continue;
+				}
+				String filePath = nombreArchivo + "/" + currentFileName;
+				if (currentDir.equals("")) {
+					filePath = nombreArchivo + "/" + currentFileName;
+				}
+				if (aFile.isDirectory()) {
+					// remove the sub directory
+					eliminarCarpeta(cliente, filePath, currentDir);
+				} else {
+					// delete the file
+					boolean deleted = cliente.deleteFile(filePath);
+					if (deleted) {
+						System.out.println("DELETED the file: " + filePath);
+					} else {
+						System.out.println("CANNOT delete the file: " + filePath);
+					}
+				}
+			}
+
+			// finally, remove the directory itself
+			boolean removed = cliente.removeDirectory(dirToList);
+			if (removed) {
+				System.out.println("REMOVED the directory: " + dirToList);
+			} else {
+				System.out.println("CANNOT remove the directory: " + dirToList);
+			}
+		}
+		JOptionPane.showMessageDialog(null, nombreArchivo + " se ha elimindado correctamente");
+		// } else {
+
+		// }
 	}
 
 }
